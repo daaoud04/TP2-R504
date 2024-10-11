44f6bfdbaf6647ba1858dc00838c49f15fae7c4f
daaoud04 <daoud.ech.chaoui@gmail.com>
2024-10-11 13:16:42 +0200

    test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
 def affiche(n1, n2):
     result = ""
-    for i in range(n1, n2 + 1):  # Inclure n2
+    for i in range(n1, n2 + 1):
         if i % 15 == 0:
             result += "FrisBee"
         elif i % 3 == 0:
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,26 +1,25 @@
 import unittest
 from io import StringIO
 import sys
-from fizzbuzz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz_10_16(self):
-        captured_output = StringIO()
-        sys.stdout = captured_output
-        
-        affiche(10, 16)  # Appel de la fonction avec 10 et 16
-        
-        sys.stdout = sys.__stdout__  # Rétablir la sortie standard
-        self.assertEqual(captured_output.getvalue().strip(), "Buzz11Fizz1314FrisBee")  # Vérifiez que cela correspond
-
     def test_fizzbuzz_5_10(self):
         captured_output = StringIO()
         sys.stdout = captured_output
-        
+
         affiche(5, 10)  # Appel de la fonction avec 5 et 10
-        
-        sys.stdout = sys.__stdout__
+
+        sys.stdout = sys.__stdout__  # Rétablir la sortie standard
         self.assertEqual(captured_output.getvalue().strip(), "BuzzFizz78FizzBuzz")
+
+    def test_fizzbuzz_10_16(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+
+        affiche(10, 16)  # Appel de la fonction avec 10 et 16
+
+        sys.stdout = sys.__stdout__  # Rétablir la sortie standard
+        self.assertEqual(captured_output.getvalue().strip(), "Buzz11Fizz1314FrisBee16")
 
 if __name__ == "__main__":
     unittest.main()
